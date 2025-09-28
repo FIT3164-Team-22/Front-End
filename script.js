0020e5cc9e56a2b2766c227c444f79012261b96b
Abdullah Sabaa <abdullah.sabaa@gmail.com>
2025-09-28 13:03:10 +1000

    Uploading vegalite code for maps

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ vegaEmbed("#geographic_map", vg_1).then(function (result) {
 var vg_2 = "vegaliteLineChart.json";
 vegaEmbed("#linechart", vg_2).then(function (result) {
 }).catch(console.error);
+
+
+document.getElementById('regionselector').addEventListener('change',() => {
+    currentMapUrl = document.getElementById('regionselector').value;
+    const baseurl = "https://raw.githubusercontent.com/FIT3164-Team-22/Front-End/refs/heads/main/"
+    fullurl = baseurl + currentMapUrl
+    vegaEmbed('#geographic_map', fullurl).catch(console.error);
+});
+    
+
+
